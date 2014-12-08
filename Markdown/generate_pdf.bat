46576ed156e90c6968eac443f5a5d13c717f8439
Ethan Ruffing <ruffinge@gmail.com>
2014-12-08 16:35:29 -0500

    Added table of contents and styling to generating scripts

--- Markdown/generate_pdf.bat
+++ Markdown/generate_pdf.bat
@@ -5,5 +5,5 @@ set TITLE="General-Purpose Style Guide"
 set AUTHOR="Ethan Ruffing"
 set DATE="December 8, 2014"
 
-pandoc styleguide.md -s -S -o styleguide.pdf --chapters -M author=%AUTHOR% -M title=%TITLE% -M date=%DATE%
-pandoc styleguide.md -s -S -o styleguide.html --chapters -M author=%AUTHOR% -M title=%TITLE% -M date=%DATE%
+pandoc styleguide.md -s -S -o styleguide.pdf --chapters -M author=%AUTHOR% -M title=%TITLE% -M date=%DATE% --toc --toc-depth=2
+pandoc styleguide.md -s -S -o styleguide.html --chapters -M author=%AUTHOR% -M title=%TITLE% -M date=%DATE% --toc --toc-depth=2 -c github-pandoc.css
