722a3b7591ef4c0f469dac6765a5c8a1d29a7fc0
Ethan Ruffing <ruffinge@gmail.com>
2015-03-03 11:53:02 -0500

    Updated settings in generate scripts

--- generate.sh
+++ generate.sh
@@ -3,9 +3,9 @@
 # This script will generate a PDF version of the style guide from the Markdown
 # version using Pandoc.
 
-TITLE="General-Purpose Style Guide"
+TITLE="C Style Guide"
 AUTHOR="Ethan Ruffing"
-DATE="December 8, 2014"
+DATE="March 3, 2015"
 
 pandoc styleguide.md -s -S -o styleguide.pdf --chapters -M author="$AUTHOR" -M title="$TITLE" -M date="$DATE" --toc --toc-depth=2
 pandoc styleguide.md -s -S -o styleguide.html --chapters -M author="$AUTHOR" -M title="$TITLE" -M date="$DATE" --toc --toc-depth=2 -c css/github-pandoc.css
