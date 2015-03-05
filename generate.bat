f5d3cea6a89135f2281bd97c3c129b02e876a18b
Ethan Ruffing <ruffinge@gmail.com>
2015-03-05 13:00:10 -0500

    Updated dates in generate scripts.

--- generate.bat
+++ generate.bat
@@ -3,7 +3,7 @@
 
 set TITLE="C Style Guide"
 set AUTHOR="Ethan Ruffing"
-set DATE="March 3, 2015"
+set DATE="March 5, 2015"
 
 pandoc styleguide.md -s -S -o styleguide.pdf --chapters -M author=%AUTHOR% -M title=%TITLE% -M date=%DATE% --toc --toc-depth=2
 pandoc styleguide.md -s -S -o styleguide.html --chapters -M author=%AUTHOR% -M title=%TITLE% -M date=%DATE% --toc --toc-depth=2 -c css/github-pandoc.css
