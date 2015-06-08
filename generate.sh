d027fa405404b350aa68471030d368cba6b5181d
Nicholas Muggio <nicholasmuggio@gmail.com>
2015-06-08 14:53:47 -0400

    generate.sh now responds to the --no-pdf flag properly.

--- generate.sh
+++ generate.sh
@@ -7,5 +7,7 @@ TITLE="C Style Guide"
 AUTHOR="Ethan Ruffing"
 DATE="March 5, 2015"
 
-pandoc styleguide.md -s -S -o styleguide.pdf --chapters -M author="$AUTHOR" -M title="$TITLE" -M date="$DATE" --toc --toc-depth=2
+if [[ $# -eq 0 ]] || [[ ! "$1" == "--no-pdf" ]]; then
+	pandoc styleguide.md -s -S -o styleguide.pdf --chapters -M author="$AUTHOR" -M title="$TITLE" -M date="$DATE" --toc --toc-depth=2
+fi
 pandoc styleguide.md -s -S -o styleguide.html --chapters -M author="$AUTHOR" -M title="$TITLE" -M date="$DATE" --toc --toc-depth=2 -c css/github-pandoc.css
